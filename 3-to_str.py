208c066543fed541d63d12eabec441d9b85edc37
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-07-28 16:36:11 +0200

    add the function that defines a type-annotated function to convert a float to a string

---
+++ 3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module that defines a type-annotated function to convert a float to a string."""
+
+
+def to_str(n: float) -> str:
+    """Return the string representation of a float.
+
+    Args:
+        n (float): The number to convert.
+
+    Returns:
+        str: The string representation of the float.
+    """
+    return str(n)
