0da281b4a4e0161dd02a299b6796d4ec4156aeb9
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-07-30 15:03:57 +0200

    correction of pycodestyle

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
-"""Module that defines a function returning an asyncio Task from wait_random."""
+"""Module that provides a function returning an asyncio.Task."""
 
 import asyncio
+
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
-    """Return an asyncio.Task that runs wait_random with the given max_delay.
+    """
+    Returns an asyncio.Task that runs wait_random with max_delay.
 
     Args:
-        max_delay (int): Maximum delay in seconds.
+        max_delay (int): Maximum delay to pass to wait_random.
 
     Returns:
-        asyncio.Task: The asyncio Task object wrapping wait_random coroutine.
+        asyncio.Task: The created asyncio Task.
     """
     return asyncio.create_task(wait_random(max_delay))
