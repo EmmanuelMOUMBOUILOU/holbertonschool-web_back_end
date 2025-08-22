38b0ad9dcb2bbd8ab8565a27d4c78850497a5b98
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-08-22 20:00:11 +0200

    fixed of the task

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,8 +1,6 @@
 // 1️ - Déclare les classes avant de créer les instances
 export class HolbertonClass {
   constructor(year, location) {
-    if (typeof year !== 'number') throw new TypeError('Year must be a number');
-    if (typeof location !== 'string') throw new TypeError('Location must be a string');
     this._year = year;
     this._location = location;
   }
@@ -18,10 +16,6 @@ export class HolbertonClass {
 
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
-    if (typeof firstName !== 'string') throw new TypeError('First name must be a string');
-    if (typeof lastName !== 'string') throw new TypeError('Last name must be a string');
-    if (!(holbertonClass instanceof HolbertonClass)) throw new TypeError('holbertonClass must be a HolbertonClass');
-
     this._firstName = firstName;
     this._lastName = lastName;
     this._holbertonClass = holbertonClass;
@@ -50,5 +44,7 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
+const listOfStudents = [student1, student2, student3, student4, student5];
+
 // 3️ - Exporte la liste des étudiants
-export const listOfStudents = [student1, student2, student3, student4, student5];
+export default listOfStudents;
