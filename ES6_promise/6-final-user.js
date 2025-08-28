ed63f8cd5c6cd99ed4c8921c980289ffe3d97337
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-08-28 22:57:38 +0200

    Improved version

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,14 +1,23 @@
+// 6-final-user.js
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName)
+    uploadPhoto(fileName),
   ]).then((results) =>
-    results.map((res) => ({
-      status: res.status,
-      value: res.status === 'fulfilled' ? res.value : res.reason,
-    }))
+    results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return {
+          status: result.status,
+          value: result.value,
+        };
+      }
+      return {
+        status: result.status,
+        value: result.reason,
+      };
+    })
   );
 }
