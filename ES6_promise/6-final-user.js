756beca3f2d1acf443d0fc95f3b32ec03f8494db
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-08-28 22:40:39 +0200

    add of the function named handleProfileSignup

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,10 @@
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpPromise, uploadPromise])
+    .then((results) => results);
+}
