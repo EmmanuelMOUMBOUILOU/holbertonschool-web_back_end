2e5356b5a28c198a8809ade101187abfb5289701
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-09-05 23:08:17 +0200

    Ajout de la fonction readDatabase pour lire et structurer les données des étudiants à partir d'un fichier

---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,21 @@
+import fs from 'fs';
+
+export function readDatabase(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) return reject(new Error('Cannot load the database'));
+
+      const lines = data.split('\n').filter(line => line.trim() !== '');
+      const students = lines.slice(1); // enlever l'en-tête
+
+      const fields = {};
+      students.forEach((line) => {
+        const [firstname,, , field] = line.split(',');
+        if (!fields[field]) fields[field] = [];
+        fields[field].push(firstname);
+      });
+
+      resolve(fields);
+    });
+  });
+}
