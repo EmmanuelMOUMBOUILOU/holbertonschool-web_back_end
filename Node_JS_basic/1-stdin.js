42a190d8740f71e12e1474ed0893f83574eb9f84
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-09-29 00:21:37 +0200

    Correction de la syntaxe et amélioration de la lisibilité dans plusieurs fichiers

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,19 +1,16 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
-process.stdin.resume();
 
 process.stdin.on('data', (data) => {
   const name = data.trim();
-  // Attention ici au \r
-  process.stdout.write(`Your name is: ${name}\r`);
-  
-  // Ferme proprement pour le cas d’un pipe
-  if (!process.stdin.isTTY) {
-    process.stdin.emit('end');
-  }
+  console.log(`Your name is: ${name}`);
 });
 
+// Laisser Node gérer automatiquement la fermeture
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
 });
+
+// Nécessaire pour permettre à stdin de fonctionner dans tous les cas
+process.stdin.resume();
