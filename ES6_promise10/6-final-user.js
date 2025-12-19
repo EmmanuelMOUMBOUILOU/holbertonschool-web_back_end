d52f134372a4050f2832cfd00ceb8f54e5ff4f9a
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-12-19 15:48:41 +0100

    Ajout de la fonction handleProfileSignup pour gérer l'inscription des utilisateurs et le téléchargement de photos avec des promesses

---
+++ ES6_promise10/6-final-user.js
@@ -0,0 +1,23 @@
+// 6-final-user.js
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) =>
+    results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return {
+          status: result.status,
+          value: result.value,
+        };
+      }
+      return {
+        status: result.status,
+        value: result.reason,
+      };
+    })
+  );
+}
