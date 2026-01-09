0142ca4e62f5d7dd8a6dc2496d83e6d9c9cca881
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2026-01-09 06:01:58 +0100

    Refactor la gestion de l'entrée utilisateur dans 1-stdin.js pour simplifier l'affichage du nom et améliorer la fermeture du processus.

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,15 +1,11 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
+process.stdin.setEncoding('utf-8');
+
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
-
-  // Fermer le stdin si l'entrée vient d'un pipe
-  if (!process.stdin.isTTY) process.exit();
-
-  // Sinon, demander à l'utilisateur de fermer manuellement (Ctrl+D)
+  process.stdout.write(`Your name is: ${data}`);
 });
 
-process.on('exit', () => {
-  console.log('This important software is now closing');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
