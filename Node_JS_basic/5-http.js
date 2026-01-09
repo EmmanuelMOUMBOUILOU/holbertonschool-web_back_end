c430a0da81086957c1886e5f80f48544a076e0fb
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2026-01-09 06:17:24 +0100

    Refactor HTTP response handling to improve readability and simplify the code.

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -6,24 +6,21 @@ const app = http.createServer((req, res) => {
 
   if (req.url === '/') {
     res.statusCode = 200;
-    res.end('Hello Holberton School!\n');
-    return;
-  }
-
-  if (req.url === '/students') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
     res.statusCode = 200;
     res.write('This is the list of our students\n');
 
     const database = process.argv[2];
 
     if (!database) {
-      res.end('Cannot load the database\n');
+      res.end('Cannot load the database');
       return;
     }
 
     fs.readFile(database, 'utf-8', (err, data) => {
       if (err) {
-        res.end('Cannot load the database\n');
+        res.end('Cannot load the database');
         return;
       }
 
@@ -32,18 +29,17 @@ const app = http.createServer((req, res) => {
         .split('\n')
         .filter((line) => line.length > 0);
 
-      const studentsData = lines.slice(1); // ignore header
+      const students = lines.slice(1);
 
       const fields = {};
-
-      studentsData.forEach((line) => {
+      students.forEach((line) => {
         const [firstname, , , field] = line.split(',');
         if (!fields[field]) fields[field] = [];
         fields[field].push(firstname);
       });
 
-      const totalStudents = studentsData.length;
-      res.write(`Number of students: ${totalStudents}\n`);
+      const total = students.length;
+      res.write(`Number of students: ${total}\n`);
 
       Object.keys(fields).forEach((field) => {
         res.write(
@@ -53,11 +49,10 @@ const app = http.createServer((req, res) => {
 
       res.end();
     });
-    return;
+  } else {
+    res.statusCode = 404;
+    res.end();
   }
-
-  res.statusCode = 404;
-  res.end();
 });
 
 app.listen(1245);
